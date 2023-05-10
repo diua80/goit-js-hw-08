8deee3a1c732815ebd9d50d3a5582727b4844f16
Iryna Yatsenko <iryna.didova@gmail.com>
2023-05-11 00:07:20 +0300

    1 and 2 tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+import storage from './storage';
 
+
+const iframe = document.getElementById('vimeo-player');
+
+const player = new Player(iframe);
+
+const LOCALKEY = 'timevideo';
+
+
+const onPlay = throttle(function(data){
+    
+    const currentTime = data.seconds;
+    
+    console.log('Current time:', currentTime);
+
+}, 1000);
+
+player.on('timeupdate', onPlay);
